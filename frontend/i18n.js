2b252410e658c8db59ff1b81982f8a776f568710
adarsh1278 <aniltiwareee@gmail.com>
2025-05-18 20:18:08 +0530

    ts error resolving

---
+++ frontend/i18n.js
@@ -0,0 +1,14 @@
+export const defaultLocale = 'en';
+export const locales = ['en', 'hi', 'gu', 'mr']; // English, Hindi, Gujarati, Marathi
+
+export function getMessages(locale) {
+  return {
+    // Define your translations here
+  };
+}
+
+export default {
+  defaultLocale,
+  locales,
+  getMessages
+};
